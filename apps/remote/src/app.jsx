907c9187715c5df8c5cc8b1cee634fbef1fd4c4a
Stanislav Ussov <stanislav.ussov@livesport.eu>
2025-01-26 18:46:14 +0100

    routes

---
+++ apps/remote/src/app.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter, Link, Routes, Route } from 'react-router-dom';
+import PageA from './pages/page-a';
+import PageB from './pages/page-b';
+import Home from './pages/home';
+import './styles.css';
+
+export default function App() {
+  return (
+    <div className="app">
+      <h2>This the root view in Remote</h2>
+      <nav className="horizontal-nav">
+        <Link to="page-a">Remote/Page A</Link>
+        <Link to="jsx/page-b">Remote/Page B</Link>
+      </nav>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="page-a" element={<PageA />} />
+        <Route path="jsx">
+            <Route path="page-b" element={<PageB />} />         
+        </Route>
+      </Routes>
+    </div>
+  );
+}
